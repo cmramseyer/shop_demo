a6f59a155d484f01e6c4cdc54cf38469e8533164
Carlos Ramseyer <carlos@bytelion.com>
2019-08-24 19:47:43 -0300

    custom query and added search form to results page

--- app/models/search.rb
+++ app/models/search.rb
@@ -5,6 +5,29 @@ class Search < ApplicationRecord
   MODELS_TO_SEARCH = [Product, Review]
 
   def self.with_keywords(keywords, opts = {})
-    Elasticsearch::Model.search(keywords, MODELS_TO_SEARCH).records
+    Elasticsearch::Model.search(search_query(keywords), MODELS_TO_SEARCH).records
+  end
+
+  def self.search_query(keywords)
+    {
+      query: {
+        function_score: { 
+          query: {
+            bool: {
+              must: [multi_match(keywords)]
+            }
+          }
+        }
+      }
+    }
+  end
+  def self.multi_match(keywords)
+    {
+      multi_match: {
+        query: keywords,
+        fields: %w[product_name^2 product_description category_name review_title^2 review_content^2 review_user review_product^2 review_product_category],
+        fuzziness: 'auto'
+      }
+    }
   end
 end
