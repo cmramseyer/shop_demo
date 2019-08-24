9a444ca9f08ebcc283055c8adcaee23a633ee53e
Carlos Ramseyer <carlos@bytelion.com>
2019-08-24 20:16:56 -0300

    search form for all pages

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,4 @@
 class HomeController < ApplicationController
 	def index
-		@search = Search.new
 	end
 end
--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -2,8 +2,6 @@ class ResultsController < ApplicationController
   def index
     @search = Search.find(params[:search_id])
     @keywords = @search.keywords
-    @new_search = Search.new
-
     @records = Search.with_keywords(@keywords)
   end
 end
