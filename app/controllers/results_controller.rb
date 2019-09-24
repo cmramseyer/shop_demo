16e8fb3baae2250fd55ed8cdd84a51b4bb417b16
Carlos Ramseyer <carlos@bytelion.com>
2019-09-24 16:44:41 -0300

    aggregation filters working, needs refactorization, ugly code

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -3,12 +3,23 @@ class ResultsController < ApplicationController
     @search = Search.find(params[:search_id])
     @keywords = @search.keywords
 
-    opts = {}
-    opts[:brand] = params[:brand]
-    opts[:category_name] = params[:category_name]
+    session[:brand] = nil unless params[:keep]
+    session[:category_name] = nil unless params[:keep]
+
+    session[:brand] ||= params[:brand]
+    session[:category_name] ||= params[:category_name]
+
+    session[:brand] = nil if params[:remove_brand]
+    session[:category_name] = nil if params[:remove_category_name]
+
+    @opts = {}
+    @opts[:brand] = session[:brand]
+    @opts[:category_name] = session[:category_name]
 
 
-    search_query = SearchQuery.new(@keywords, opts)
+
+
+    search_query = SearchQuery.new(@keywords, @opts)
     elastic_records = search_query.records
     @categories = search_query.aggregations.categories.buckets
     @brands = search_query.aggregations.brands.buckets
