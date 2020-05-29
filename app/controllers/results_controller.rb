bebf6418763ee22238246426896bce1ad075236c
Carlos Ramseyer <cmramseyer@gmail.com>
2020-05-28 21:33:50 -0300

    removed deprecated method for session handler

--- app/controllers/results_controller.rb
+++ app/controllers/results_controller.rb
@@ -13,21 +13,4 @@ class ResultsController < ApplicationController
     @records = elastic_records.map { |r| Searchable.create(r) }
   end
 
-  private
-
-  def set_opts_filters
-    @opts = {}
-    [:brand, :category_name, :type].each do |key|
-      # each filter link sends a keep params
-      # we will delete session variables if keep it's not present
-      session[key] = nil unless params[:keep]
-      # we will store the param value if session var is not present
-      session[key] ||= params[key]
-      # we will delete the session value if the remove param is present
-      session[key] = nil if params["remove_#{key}".to_sym]
-      # now we assign the session value into an instance variable
-      @opts[key] = session[key]
-    end
-
-  end
 end
